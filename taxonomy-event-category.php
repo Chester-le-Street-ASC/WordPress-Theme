e8bcc956e87419c60f30638414ad3f74b777ed05
Chris Heppell <clheppell@icloud.com>
2018-07-21 16:25:59 +0100

    Show address for gala venues

--- taxonomy-event-category.php
+++ taxonomy-event-category.php
@@ -22,32 +22,34 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
-    	<div class="col-lg-8">
+  	<div class="col-lg-8">
 
-<div id="primary" role="main" class="content-area">
+      <div id="primary" role="main" class="content-area">
 
-	<!-- Page header, display category title-->
-		<h1 class="mb-0">
-			<?php printf( __( 'Gala Category: %s', 'eventorganiser' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?>
-		</h1>
-        
+      	<!-- Page header, display category title-->
+    		<h1 class="mb-0">
+    			<?php printf( __( '%s Galas', 'eventorganiser' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?>
+    		</h1>
+
+    		<!-- If the category has a description display it-->
+    		<?php
+    		$category_description = category_description();
+    		if ( ! empty( $category_description ) ) {
+    			echo apply_filters( 'category_archive_meta', '<div class="lead">' . $category_description . '</div>' );
+    		}
+    		?>
+
         <hr>
 
-		<!-- If the category has a description display it-->
-		<?php
-		$category_description = category_description();
-		if ( ! empty( $category_description ) ) {
-			echo apply_filters( 'category_archive_meta', '<div class="lead">' . $category_description . '</div>' );
-		}
-		?>
+      	<?php eo_get_template_part( 'eo-loop-events' ); //Lists the events ?>
 
-	<?php eo_get_template_part( 'eo-loop-events' ); //Lists the events ?>
-	
-</div><!-- #primary -->
-</div>
+      </div><!-- #primary -->
+    </div>
 
-<!-- Call template sidebar and footer -->
-<div class="col-lg-4"><?php get_sidebar(); ?></div>
-</div>
+    <!-- Call template sidebar and footer -->
+    <div class="col-lg-4">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
 </div>
 <?php get_footer();
